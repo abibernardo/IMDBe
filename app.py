2340e23e7171e91fb2ba5363fc3025461d86119f
abibernardo <153117023+abibernardo@users.noreply.github.com>
2026-08-04 20:39:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -508,38 +508,28 @@ st.markdown(
 
 def artigo(titulo, subtitulo, url):
 
-    st.markdown("<div class='artigo'>", unsafe_allow_html=True)
-
     st.markdown(
         f"""
-        <div class="artigo-titulo">
-            <a href="{url}" target="_blank">{titulo}</a>
+        <div class="artigo">
+
+            <div class="artigo-titulo">
+                <a href="{url}" target="_blank">
+                    {titulo}
+                </a>
+            </div>
+
+            <div class="artigo-subtitulo">
+                {subtitulo}
+            </div>
+
+            <a class="artigo-link" href="{url}" target="_blank">
+                Ler no Substack →
+            </a>
+
         </div>
         """,
         unsafe_allow_html=True
     )
-
-    st.markdown(
-        f"""
-        <div class="artigo-subtitulo">
-            {subtitulo}
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-    st.markdown(
-        f"""
-        <a class="artigo-link"
-           href="{url}"
-           target="_blank">
-            Ler no Substack →
-        </a>
-        """,
-        unsafe_allow_html=True
-    )
-
-    st.markdown("</div>", unsafe_allow_html=True)
 
 artigo(
     "Twin Peaks: De fora pra dentro",
